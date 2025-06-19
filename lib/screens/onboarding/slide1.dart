a8bb30b7f3f78e5c09530ed5edfdc9b665bdcba7
Eva Azizah <eva.azizah@example.com>
2025-06-19 22:48:23 +0700

    perbaikan kode

--- lib/screens/onboarding/slide1.dart
+++ lib/screens/onboarding/slide1.dart
@@ -8,30 +8,65 @@ class Slide1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.lightBlue[100],
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        children: [
-          const SizedBox(height: 40),
-          Image.asset('assets/icons/onboarding1.jpg', height: 250),
-          const SizedBox(height: 32),
-          const Text(
-            'Organize Your Day',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
+      color: const Color.fromARGB(255, 241, 240, 240),
+
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+
+              Text(
+                'Organize Your Day',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+
+              Text(
+                'Keep track of your tasks,\nwith smart reminders and scheduling.',
+                style: TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 250),
+
+              Image.asset(
+                'assets/icons/onboarding1.jpg',
+                height: 250,
+                fit: BoxFit.contain,
+              ),
+              const Spacer(),
+
+              Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                  width: 100,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: onNext,
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(7.0),
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    child: const Text(
+                      'Next',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 16),
-          const Text(
-            'Keep track of your tasks,\nwith smart reminders and scheduling.',
-            style: TextStyle(fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-          const Spacer(),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: ElevatedButton(onPressed: onNext, child: const Text("Next")),
-          ),
-        ],
+        ),
       ),
     );
   }
